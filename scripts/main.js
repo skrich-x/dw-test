3f223754ebc82477b33f4839930ab60937d63e1f
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 22:08:57 -0400

    Implement changes to set required fields with js: -adjusted id's to snake_case ( parser interprets as subtract operator ) 	-could also use brackets -created Rules and Messages objects

--- scripts/main.js
+++ scripts/main.js
@@ -22,20 +22,52 @@
     });
 
 
-  //// Use jquery validation for valid email ////
+  //// Use jquery validation to create rules and messages objects ////
 
     $("#shipping-form").validate({
       rules: {
-        emailAddress: {
+    ///confirm email is valid and confirmation matches///
+        email_address: {
           required: true,
           email: true
         },
 
-        emailAddressConfirm: {
+        email_address_confirm: {
           required: true,
           email: true,
           equalTo: "#emailAddress"
-        }
+        },
+
+        phone: {
+          required: true,
+          phoneUS: true
+        },
+
+    /// Other Required Fields ///
+        first_name:"required",
+        last_name:"required",
+        address_1:"required",
+        city:"required",
+        zip:"required",
+      },
+
+    /// Define error messages if left blank or is invalid
+      messages: {
+        email: {
+          email: "Please Enter a Valid Email"
+        },
+
+        email_address_confirm: {
+          required: "Must Confirm Email",
+          email_address_confirm: "Emails Must Match"
+        },
+
+        first_name:"Please enter your first name.",
+        last_name:"Please enter your last name.",
+        address_1:"Please provide your address.",
+        city: "Please provide your city.",
+        zip: "Please provide your Zip Code.",
+        phone: "Please provide a valid US phone number"
       }
     });
   });
