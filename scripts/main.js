24242e35b1699b8664478ab12d11e3113cf314bc
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 20:54:54 -0400

    Added Email input attribute -added comment to JS for clarity

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,8 @@
   'use strict';
 
   $(document).ready(function(){
-  
+
+  //// Hide Company Input field on page load / Show on click of Radio button
     $("#shipping-company").hide();
       $("#commercial").click(function(){
         $("#shipping-company").show();
