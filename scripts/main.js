6a1cb61c68337ab94f2d051e2af62f41d8407db7
Kyle Richardson <s.k.richardson7@gmail.com>
2015-09-05 20:27:32 -0400

    Remove Company Field on Page load -changed background color

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
-console.log('working');
+(function(){
+  'use strict';
+
+  $(document).ready(function(){
+  
+    $("#shipping-company").hide();
+      $("#commercial").click(function(){
+        $("#shipping-company").show();
+    });
+
+  });
+})();
